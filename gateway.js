8f6397b027bc02fb647941ef9b83e9ac59ed6743
Max Struever <struever.max@bcgplatinion.com>
2021-03-04 10:39:18 -0500

    added scheams

--- gateway.js
+++ gateway.js
@@ -11,7 +11,10 @@ const gateway = new ApolloGateway({
     { name: "accounts", url: "http://localhost:4001/graphql" },
     { name: "reviews", url: "http://localhost:4002/graphql" },
     { name: "products", url: "http://localhost:4003/graphql" },
-    { name: "inventory", url: "http://localhost:4004/graphql" }
+    { name: "inventory", url: "http://localhost:4004/graphql" },
+    { name: "rickandmorty", url: "https://rickandmortyapi.com/graphql" },
+    // { name: "rickandmorty", url: "https://separated-coffee-trade.glitch.me/",  },
+    // { name: "hasuratest", url: "https://hasura-gql-wrapper.glitch.me/" },
   ],
 
   // Experimental: Enabling this enables the query plan view in Playground.
